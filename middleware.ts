f18ecbb41c84149eff139e6aa824bb3d7d3e3254
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2024-06-22 19:20:21 -0400

    comments

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 export { default } from 'next-auth/middleware';
 
+// Redirect to / if any of these are accessed when session is null
 export const config = { matcher: [
   '/dashboard',
   '/dashboard/school', 
