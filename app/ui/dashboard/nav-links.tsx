14329b4ce1520e9dde3c63a6097e86fbc7e94117
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2024-06-25 12:39:56 -0400

    sidebar close fix

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -169,7 +169,7 @@ export default function NavLinks({
             )}
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            {!collapsed ? <p className="hidden md:block">{link.name}</p> : <></>}
           </button>
         );
       }
